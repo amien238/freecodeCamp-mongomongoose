a2fe3d8201bdc113a0355abfbb2c2ff53d8e6eca
amien238 <amien23804@gmail.com>
2025-08-06 14:39:37 +0700

    find people by name

--- myApp.js
+++ myApp.js
@@ -47,8 +47,12 @@ const createManyPeople = (arrayOfPeople, done) => {
 
 
 const findPeopleByName = (personName, done) => {
-  done(null /*, data*/);
+  Person.find({ name: personName }, (err, data) => {
+    if (err) return done(err);
+  done(null, data);
+  });
 };
+findPeopleByName("Alice", console.log);
 
 const findOneByFood = (food, done) => {
   done(null /*, data*/);
